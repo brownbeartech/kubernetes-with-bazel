dc5e7e6ac678e0649da0c994c49e1825fdef6d81
mattjs <mail@mattjs.us>
2018-12-04 17:44:40 -0700

    Add undertow

---
+++ WORKSPACE
@@ -0,0 +1,24 @@
+maven_jar(
+    name = "io_undertow_core",
+    artifact = "io.undertow:undertow-core:2.0.13.Final"
+)
+
+maven_jar(
+    name = "io_undertow_websockets_jsr",
+    artifact = "io.undertow:undertow-websockets-jsr:2.0.13.Final"
+)
+
+maven_jar(
+    name = "org_jboss_xnio_api",
+    artifact = "org.jboss.xnio:xnio-api:3.3.8.Final"
+)
+
+maven_jar(
+    name = "org_jboss_xnio_nio",
+    artifact = "org.jboss.xnio:xnio-nio:3.3.8.Final"
+)
+
+maven_jar(
+    name = "org_jboss_logging",
+    artifact = "org.jboss.logging:jboss-logging:3.3.2.Final"
+)
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 package src;
 
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Undertow undertow = Undertow.builder()
+            .addHttpListener(4567, "0.0.0.0")
+            .setHandler(new HttpHandler() {
+                @Override
+                public void handleRequest(HttpServerExchange exchange) throws Exception {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseSender().send("Hello World");
+                }
+            })
+            .build();
+        undertow.start();
     }
 }
